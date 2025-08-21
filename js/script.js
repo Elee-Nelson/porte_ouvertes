ee55ab89900ec35464cc720d60604d81c7f873db
Elee-Nelson Dégarie <6221493@cstj.qc.ca>
2025-08-21 09:36:07 -0400

    bouton cliquable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("lien est fait");
+const boutons = document.querySelector(".bouton");
+
+// for (let i=0; i < boutons.length; i++) {
+//     boutons[i].addEventListener("click", e=> {
+//         e.preventDefault();
+//         console.log("touché");
+//     })
+// }
+
+boutons.addEventListener("click", e=> {
+    e.preventDefault();
+    console.log("touché");
+})
